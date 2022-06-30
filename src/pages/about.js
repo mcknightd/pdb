17f1b6d83bcd56cd5c76ac257effbc984cf7d615
D McKnight <dmcknight@fosforus.com>
2022-06-30 17:40:14 -0500

    pt2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Page">
+      <p>test about</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
